a0951697c56c354f6fcdc6da06a337aedaf4d62c
vet1ments <vet1ments@naver.com>
2024-09-30 21:06:49 +0000

    v3.0.10 update

--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.23.1
 require (
 	buf.build/gen/go/bufbuild/protovalidate/protocolbuffers/go v1.34.2-20240920164238-5a7b106cbb87.2
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.22.0
-	google.golang.org/genproto/googleapis/api v0.0.0-20240924160255-9d4c2d233b61
+	google.golang.org/genproto/googleapis/api v0.0.0-20240930140551-af27646dc61f
 	google.golang.org/grpc v1.67.0
 	google.golang.org/protobuf v1.34.2
 )
@@ -14,5 +14,5 @@ require (
 	golang.org/x/net v0.28.0 // indirect
 	golang.org/x/sys v0.24.0 // indirect
 	golang.org/x/text v0.17.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20240903143218-8af14fe29dc1 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240924160255-9d4c2d233b61 // indirect
 )
--- v1/user_grpc.pb.go
+++ v1/user_grpc.pb.go
@@ -23,6 +23,7 @@ const (
 	UserService_GetAccessTokenInfo_FullMethodName = "/grpcoauth.v1.UserService/GetAccessTokenInfo"
 	UserService_GetUserMe_FullMethodName          = "/grpcoauth.v1.UserService/GetUserMe"
 	UserService_UserLogout_FullMethodName         = "/grpcoauth.v1.UserService/UserLogout"
+	UserService_GetUserList_FullMethodName        = "/grpcoauth.v1.UserService/GetUserList"
 )
 
 // UserServiceClient is the client API for UserService service.
@@ -32,6 +33,7 @@ type UserServiceClient interface {
 	GetAccessTokenInfo(ctx context.Context, in *emptypb.Empty, opts ...grpc.CallOption) (*GetAccessTokenInfoResponse, error)
 	GetUserMe(ctx context.Context, in *emptypb.Empty, opts ...grpc.CallOption) (*GetUserMeResponse, error)
 	UserLogout(ctx context.Context, in *emptypb.Empty, opts ...grpc.CallOption) (*UserLogoutResponse, error)
+	GetUserList(ctx context.Context, in *emptypb.Empty, opts ...grpc.CallOption) (*GetUserListResponse, error)
 }
 
 type userServiceClient struct {
@@ -72,6 +74,16 @@ func (c *userServiceClient) UserLogout(ctx context.Context, in *emptypb.Empty, o
 	return out, nil
 }
 
+func (c *userServiceClient) GetUserList(ctx context.Context, in *emptypb.Empty, opts ...grpc.CallOption) (*GetUserListResponse, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(GetUserListResponse)
+	err := c.cc.Invoke(ctx, UserService_GetUserList_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // UserServiceServer is the server API for UserService service.
 // All implementations must embed UnimplementedUserServiceServer
 // for forward compatibility.
@@ -79,6 +91,7 @@ type UserServiceServer interface {
 	GetAccessTokenInfo(context.Context, *emptypb.Empty) (*GetAccessTokenInfoResponse, error)
 	GetUserMe(context.Context, *emptypb.Empty) (*GetUserMeResponse, error)
 	UserLogout(context.Context, *emptypb.Empty) (*UserLogoutResponse, error)
+	GetUserList(context.Context, *emptypb.Empty) (*GetUserListResponse, error)
 	mustEmbedUnimplementedUserServiceServer()
 }
 
@@ -97,6 +110,9 @@ func (UnimplementedUserServiceServer) GetUserMe(context.Context, *emptypb.Empty)
 }
 func (UnimplementedUserServiceServer) UserLogout(context.Context, *emptypb.Empty) (*UserLogoutResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method UserLogout not implemented")
+}
+func (UnimplementedUserServiceServer) GetUserList(context.Context, *emptypb.Empty) (*GetUserListResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetUserList not implemented")
 }
 func (UnimplementedUserServiceServer) mustEmbedUnimplementedUserServiceServer() {}
 func (UnimplementedUserServiceServer) testEmbeddedByValue()                     {}
@@ -173,6 +189,24 @@ func _UserService_UserLogout_Handler(srv interface{}, ctx context.Context, dec f
 	return interceptor(ctx, in, info, handler)
 }
 
+func _UserService_GetUserList_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(emptypb.Empty)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(UserServiceServer).GetUserList(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: UserService_GetUserList_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(UserServiceServer).GetUserList(ctx, req.(*emptypb.Empty))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // UserService_ServiceDesc is the grpc.ServiceDesc for UserService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -191,6 +225,10 @@ var UserService_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "UserLogout",
 			Handler:    _UserService_UserLogout_Handler,
+		},
+		{
+			MethodName: "GetUserList",
+			Handler:    _UserService_GetUserList_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
